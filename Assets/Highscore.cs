99680dbaabfeb590dafda84199c1046cb3f5c4e9
Marco Burstein <marco@marco.how>
2018-05-27 06:59:23 -0700

    Create the highscore text.

---
+++ Assets/Highscore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour {
+	public Score score;
+	public Text highscoreText;
+
+	[HideInInspector]
+	public int highscore;
+
+	private int originalHighscore;
+
+	void Start() {
+		if (PlayerPrefs.HasKey("highscore")) {
+			originalHighscore = PlayerPrefs.GetInt("highscore");
+			highscore = originalHighscore;
+		} else {
+			originalHighscore = -1;
+			highscore = -1;
+		}
+	}
+	
+	void FixedUpdate() {
+		if (score.currentScore <= originalHighscore) {
+			highscoreText.text = "HS: " + originalHighscore.ToString();
+		} else {
+			highscore = score.currentScore;
+			highscoreText.text = "New High Score!";
+		}
+	}
+
+	void OnDestroy() {
+		if (originalHighscore < highscore)
+			PlayerPrefs.SetInt("highscore", highscore);
+	}
+}
