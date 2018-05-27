55547ecbee14dd7a785495f3257f4bbcf1add834
Marco Burstein <marco@marco.how>
2018-05-26 21:25:10 -0700

    Create the score text.

---
+++ Assets/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	/*
+	    These values are used to create the score function of time, 
+	    `s(t) = at^2 + bt + c`.
+	 */
+	public float scoreFunctionA;
+	public float scoreFunctionB;
+	public float scoreFunctionC;
+
+	public Text scoreText;
+
+	[HideInInspector]
+	public int currentScore = 0;
+
+	void Start () {}
+	void FixedUpdate () {
+		currentScore = ScoreForFixedTime(Time.fixedTime);
+		scoreText.text = currentScore.ToString();
+	}
+
+	int ScoreForFixedTime (float fixedTime) {
+		return (int) (
+		    scoreFunctionA * Mathf.Pow(fixedTime, 2)
+		   	    + scoreFunctionB * fixedTime
+		   		+ scoreFunctionC
+		);
+	}
+}
