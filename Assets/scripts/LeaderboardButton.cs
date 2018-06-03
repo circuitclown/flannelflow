a9b5bd5f5584783607c3711f144acd3e40c422a3
Marco Burstein <marco@marco.how>
2018-06-02 19:23:01 -0700

    Configure the Unity Remote.

--- Assets/scripts/LeaderboardButton.cs
+++ Assets/scripts/LeaderboardButton.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TheNextFlow.UnityPlugins;
 
-public class LeaderboardButton : MonoBehaviour {
-	public SceneTransitioner sceneTransitioner;
-	
+public class LeaderboardButton : MonoBehaviour {	
 	void Start () {}
 	void Update() {}
 
 	public void GoToLeaderboard() {
+		MobileNativePopups.OpenAlertDialog(
+			"Sorry!", 
+			"There was an error opening the leaderboard. Please try again "
+			+ "later.", 
+			"OK", 
+			OnAcceptLeaderboardFailure
+		);
+
 		Social.localUser.Authenticate(OnAuthenticate);
 	}
 
@@ -26,4 +33,6 @@ public class LeaderboardButton : MonoBehaviour {
 			);
 		}
 	}
+
+	private void OnAcceptLeaderboardFailure() {}
 }
